ceeed53ff24fedf05f207066504b6cfe9465a0f9
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-11-12 03:35:11 +0530

    fixed home page

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,27 +1,45 @@
+"use client"
 import React from 'react'
-
+import { useState } from 'react'
 import Image from 'next/image'
+import Draggable from 'react-draggable'
 function Main() {
+  const [Showdiv, SetShowdiv] = useState(true)
   return (
     <div className='h-screen w-full overflow-hidden relative z-0 bg-white'>
-        <div>
-            <Image
-            className='absolute'
-            src='/shapes/shape_03 1.png'
-            alt=''
-            height={379}
-            width={2266}
-            />
-        </div>
-        <div>
+        <div className='h-full w-full flex items-center justify-center'>
+        {Showdiv && (
+          <Draggable>
+          <div className='bg-white border-2 border-black z-10 h-[231px] w-[586px] absolute flex items-center justify-evenly p-4'>
+            <div>
             <Image
             className=''
-            src='/shapes/shape_02 1.png'
+            src='/logo/black.png'
             alt=''
-            height={1347}
-            width={1368}
+            height={131}
+            width={94}
+            />
+            </div>
+            <div className='flex-col w-2/4 text-black'>
+              <div>Welcome To Rupture Labs</div>
+              <div>Copyright (c)Rupture Labs Disk Space Free: 19796K Memory Free:   301K</div>
+             
+            </div>
+            <div onClick={() => SetShowdiv(false)} className='absolute bottom-0 text-black text-center w-full p-4'>
+              <span className='border-2 border-black px-8 rounded-[10px] py-2 cursor-pointer'>OK</span>
+            </div>
+          </div>
+          </Draggable>
+        )}
+            <Image
+            className='z-0'
+            src='/bg/6725513 2.png'
+            alt=''
+            height={1000}
+            width={1920}
             />
         </div>
+        
     </div>
   )
 }
